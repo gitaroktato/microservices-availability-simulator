3461e77698c9939a46ef964d6d7d51a534637b25
Oresztesz Margaritisz <v-omargaritisz@hotels.com>
2019-11-11 16:13:23 +0100

    Changed some naming for the model

--- main/model.py
+++ main/model.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class ValidationException(Exception):
-    def __init__(self):
-        pass
+    def __init__(self, message):
+        super().__init__(message)
 
 class Call(Enum):
     PASS = 1
@@ -10,27 +10,28 @@ class Call(Enum):
 
 class Service:
 
-    def __init__(self, failure_rate, total_rate):
-        self.failure_rate = failure_rate
-        self.total_rate = total_rate
+    def __init__(self, failure_threshold, granularity):
+        self.failure_threshold = failure_threshold
+        self.granularity = granularity
         self.dependencies = []
 
-    def selfCall(self, passFailRange):
-        if passFailRange <= self.failure_rate:
+    def self_call(self, pass_fail):
+        if pass_fail > self.granularity:
+            raise ValidationException("Pass-fail {} is bigger than service's granularity {}"
+                .format(pass_fail, self.granularity))
+        elif pass_fail <= self.failure_threshold:
             return Call.FAIL
-        elif passFailRange <= self.total_rate and self.failure_rate < passFailRange:
+        elif pass_fail <= self.granularity and self.failure_threshold < pass_fail:
             return Call.PASS
-        else:
-            raise ValidationException
     
-    def call(self, passFailRange):
+    def call(self, pass_fail):
         for dependency in self.dependencies:
-            if dependency.call(passFailRange) == Call.FAIL:
+            if dependency.call(pass_fail) == Call.FAIL:
                 return Call.FAIL
-        return self.selfCall(passFailRange)
-            
+        return self.self_call(pass_fail)
 
     def add_dependency(self, dependency):
-        if (dependency.total_rate != self.total_rate):
-            raise ValidationException
+        if (dependency.granularity != self.granularity):
+            raise ValidationException("Granularity for dependency {} should be equal to the service's {}"
+                .format(dependency.granularity, self.granularity))
         self.dependencies.append(dependency)
--- test/testModel.py
+++ test/testModel.py
@@ -3,16 +3,16 @@ from main.model import Service
 from main.model import Call
 from main.model import ValidationException
 
-def create_service_which_always_succeeds():
-    return Service(0, 100)
-
 class TestService(unittest.TestCase):
 
 
+    def create_service_which_always_succeeds(self):
+        return Service(0, 100)
+
     def test_init(self):
         sut = Service(2, 1000)
-        self.assertEquals(sut.failure_rate, 2)
-        self.assertEquals(sut.total_rate, 1000)
+        self.assertEquals(sut.failure_threshold, 2)
+        self.assertEquals(sut.granularity, 1000)
 
     def test_call_with_valid_values(self):
         sut = Service(2, 1000)
@@ -30,7 +30,7 @@ class TestService(unittest.TestCase):
             pass
     
     def test_with_dependencies(self):
-        sut = create_service_which_always_succeeds()
+        sut = self.create_service_which_always_succeeds()
         dependency = Service(100, 100)
         sut.add_dependency(dependency)
         result = sut.call(1)
@@ -38,12 +38,12 @@ class TestService(unittest.TestCase):
     
     def test_with_dependencies_when_self_fails(self):
         sut = Service(100, 100)
-        dependency = create_service_which_always_succeeds()
+        dependency = self.create_service_which_always_succeeds()
         sut.add_dependency(dependency)
         result = sut.call(1)
         self.assertEquals(result, Call.FAIL)
 
-    def test_with_dependency_of_different_total_rate(self):
+    def test_with_dependency_of_different_granularity(self):
         sut = Service(2, 5)
         dependency = Service(2,30)
         try:
