a706e566b993f9337227b0e8be741883aa4981ee
Oresztesz Margaritisz <v-omargaritisz@hotels.com>
2019-11-26 09:05:38 +0100

    Simple POC that just works, needs excessive tweaking

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+# importing networkx 
+import networkx as nx 
+# importing matplotlib.pyplot 
+import matplotlib.pyplot as plt 
+
 from main.model import Service
 from main.model import Call
 from main.model import ValidationException
 
 def main():
-    entrypoint = Service(1, 100)
-    entrypoint.add_dependency(Service(1, 100))
-    entrypoint.add_dependency(Service(1, 100))
-    entrypoint.add_dependency(Service(1, 100))
-    entrypoint.add_dependency(Service(1, 100))
+    app = Service(5, 10000)
+    database = Service(5, 10000)
+    app.add_dependency(database)
     failed = 0
     cycles = 100000
     for i in range(cycles):
-        if entrypoint.call() == Call.FAIL:
+        if app.call() == Call.FAIL:
             failed += 1
     print (1 - failed/cycles)
-    
+    # Drawing graph
+    # https://www.geeksforgeeks.org/python-visualize-graphs-generated-in-networkx-using-matplotlib/
+    g = nx.Graph() 
+    g.add_edge(app, database)
+    options = {
+        'with_labels': True,
+        'node_size': 4500,
+        'width': 0.2,
+        'labels': {
+            app: 'app - %.2f' % ((1 - 5/10000) * 100),
+            database: 'database - %.2f' % ((1 - 5/10000) * 100)
+        }
+    }
+    total_availability = 'Total system availability %f' % ((1 - failed/cycles) * 100)
+    nx.draw_shell(g, **options, label = total_availability)
+    plt.show()
+
 if __name__ == '__main__':
     main()
