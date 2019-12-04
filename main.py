8c22bcc666b0539f6e1e9b921cf2be8e5b3f605c
Oresztesz Margaritisz <v-omargaritisz@hotels.com>
2019-12-04 13:31:58 +0100

    Added tree structure drawing

--- main.py
+++ main.py
@@ -9,24 +9,27 @@ from main.model import ValidationException
 
 def main():
     # Configuring microservice structure
+    proxy = Service(5, 100, 'proxy')
     aggregate = Service(5, 100, 'aggregate')
     app = Service(5, 100, 'app')
     another_app = Service(5, 100, 'another_app')
     database = Service(5, 100, 'database')
     another_app_db = Service(5, 100, 'database')
     cache = Service(5, 100, 'cache')
+    proxy.add_dependency(aggregate)
     aggregate.add_dependency(app)
     aggregate.add_dependency(another_app)
     app.add_dependency(database)
     app.add_dependency(cache)
+    another_app.add_dependency(cache)
     another_app.add_dependency(another_app_db)
     # Simulating calls in cycles
     cycles = 100000
     for _ in range(cycles):
-        aggregate.call()
+        proxy.call()
     # Drawing from root
     draw = Draw()
-    draw.draw(aggregate)
+    draw.draw(proxy)
 
 if __name__ == '__main__':
     main()
--- main/gui.py
+++ main/gui.py
@@ -1,7 +1,73 @@
 import networkx as nx 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import random
 
 from main.model import Service
+
+def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
+
+    '''
+    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.  
+    Licensed under Creative Commons Attribution-Share Alike 
+
+    If the graph is a tree this will return the positions to plot this in a 
+    hierarchical layout.
+
+    G: the graph (must be a tree)
+
+    root: the root node of current branch 
+    - if the tree is directed and this is not given, 
+      the root will be found and used
+    - if the tree is directed and this is given, then 
+      the positions will be just for the descendants of this node.
+    - if the tree is undirected and not given, 
+      then a random choice will be used.
+
+    width: horizontal space allocated for this branch - avoids overlap with other branches
+
+    vert_gap: gap between levels of hierarchy
+
+    vert_loc: vertical location of root
+
+    xcenter: horizontal location of root
+    '''
+    # if not nx.is_tree(G):
+    #     raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
+        '''
+        see hierarchy_pos docstring for most arguments
+
+        pos: a dict saying where all nodes go if they have been assigned
+        parent: parent of this branch. - only affects it if non-directed
+
+        '''
+
+        if pos is None:
+            pos = {root:(xcenter,vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)  
+        if len(children)!=0:
+            dx = width/len(children) 
+            nextx = xcenter - width/2 - dx/2
+            for child in children:
+                nextx += dx
+                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap, 
+                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
+                                    pos=pos, parent = root)
+        return pos
+
+
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
 
 class Draw:
 
@@ -18,11 +84,12 @@ class Draw:
         self.add_edges(root_service, graph, labels)
         options = dict(self.DEFAULT_OPTIONS)
         options['labels'] = labels
-        nx.draw_spring(graph, **options)
+        pos = hierarchy_pos(graph, root_service)
+        nx.draw(graph, pos, **options)
         plt.show()
 
     def add_edges(self, service, graph, labels):
-        labels[service] = '%s - %.2f' % (service.get_name(), service.get_total_availability_percentage())
+        labels[service] = '%s - %.2f%%' % (service.get_name(), service.get_total_availability_percentage())
         for dependency in service.dependencies:
             graph.add_edge(service, dependency)
             self.add_edges(dependency, graph, labels)
