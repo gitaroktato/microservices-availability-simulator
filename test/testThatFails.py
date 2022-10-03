27f70f9ad5df5fea2577a8032c5b994afc95a4e0
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2022-10-03 13:13:05 +0200

    Adding failing test to check workflow

---
+++ test/testThatFails.py
@@ -0,0 +1,10 @@
+from main.aws import EC2MultiAZ, S3, CloudFront, Route53, APIGateway, DynamoDB, EC2Instance, DynamoDBGlobalTable, SNS, \
+    ELB
+import unittest
+
+
+class TestThatFails(unittest.TestCase):
+
+    def test_fails(self):
+        self.fail("Sorry, this test should fail")
+        
